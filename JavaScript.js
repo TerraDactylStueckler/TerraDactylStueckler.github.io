a8096038ec2904d6107de8003fa746bbdaf9a4cb
TerraDactylStueckler <tstueckler@byui.edu>
2021-01-09 16:17:30 -0700

    Added Java

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+let date= document.lastModified;
+document.getElementById("lastUpdate").textContent = "Last Updated: " + date;
+
+const year = new Date();
+document.getElementById("copyYear").textContent = year.getFullYear();
+
+
