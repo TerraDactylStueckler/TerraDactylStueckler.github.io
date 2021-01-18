7346d7210040020d6845aad63105afba97c091c2
TerraDactylStueckler <tstueckler@byui.edu>
2021-01-17 18:28:13 -0700

    recovery

---
+++ lesson1/js.js
@@ -0,0 +1,5 @@
+let date= document.lastModified;
+document.getElementById("lastUpdate").textContent = "Last Updated: " + date;
+
+const year = new Date();
+document.getElementById("copyYear").textContent = year.getFullYear();
