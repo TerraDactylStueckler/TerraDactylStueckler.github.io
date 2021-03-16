2730ad86c87e77a03a73e5db6d8b4e7172e0c4dc
TerraDactylStueckler <tstueckler@byui.edu>
2021-03-16 15:11:14 -0600

    Random stuff

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -2,7 +2,7 @@ const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-   // console.log(jsObject);
+   console.log(jsObject);
     const temperature = document.querySelector('#current-temp');
     temperature.textContent = jsObject.main.temp;
 
